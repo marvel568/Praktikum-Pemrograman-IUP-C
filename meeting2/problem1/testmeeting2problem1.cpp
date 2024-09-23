c9396705ce77fa41d2f9025a0925d35e26185b96
marvel568 <marvel.prabawa@gmail.com>
2024-09-23 21:10:56 +0700

    Add files via upload

---
+++ meeting2/problem1/testmeeting2problem1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>  
+using namespace std;
+
+int main() {
+    string name;  
+    double grosssalary, installment, insurance;
+
+   
+    name = "Marvel";  
+    grosssalary = 10000;  
+    installment = 2000;
+    insurance = 1000;
+
+
+    return 0;
+}
