d0bb99dd5cde12366511e30649cb42d405a60264
marvel568 <marvel.prabawa@gmail.com>
2024-09-24 13:11:49 +0700

    Update salary_calculation.cpp

--- meeting2/problem1/salary_calculation.cpp
+++ meeting2/problem1/salary_calculation.cpp
@@ -1,44 +1,39 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <tuple>  
 using namespace std;
+
+
+tuple<string, double, double, double> getTestValues();
 
 int main() {
     string name;
-    double grosssalary, installment, insurance, netSalary;
-    double tax;
-
-
-    cout << "Insert name: ";
-    getline(cin, name);  
-    cout << "Insert gross salary: ";
-    cin >> grosssalary;
-    cout << "Insert installment: ";
-    cin >> installment;
-    cout << "Insert insurance: ";
-    cin >> insurance;
-
-    tax = grosssalary*0.2;
-    netSalary = grosssalary-tax-installment-insurance;
-
-    cout << left << setw(25) << "name"
-                 << setw(25) << "gross salary"
-                 << setw(25) << "tax"
-                 << setw(25) << "installment"
-                 << setw(25) << "insurance"
-                 << setw(25) << "net Salary" <<endl;
+    double grosssalary, installment, insurance;
     
-    cout << setfill('-') << setw(75) << "-" << setfill(' ') << endl;
-    
+    tie(name, grosssalary, installment, insurance) = getTestValues();
+
+    double tax = grosssalary * 0.2;
+    double netSalary = grosssalary - tax - installment - insurance;
+
+    cout << left << setw(25) << "Name"
+                 << setw(25) << "Gross Salary"
+                 << setw(25) << "Tax"
+                 << setw(25) << "Installment"
+                 << setw(25) << "Insurance"
+                 << setw(25) << "Net Salary" << endl;
+
+    cout << setfill('-') << setw(125) << "-" << setfill(' ') << endl;
+
     cout << left << setw(25) << name
                  << setw(25) << grosssalary
                  << setw(25) << tax
                  << setw(25) << installment
                  << setw(25) << insurance
-                 << setw(25) << netSalary <<endl;
+                 << setw(25) << netSalary << endl;
 
-
-return 0;
+    return 0;
+}
 
     
 
