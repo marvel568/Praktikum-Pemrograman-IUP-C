77443c78c39833c59b9f9c9896c1ef87d89b9661
marvel568 <marvel.prabawa@gmail.com>
2024-11-11 22:21:30 +0700

    Update makefile.cpp

--- meeting4/part1/makefile.cpp
+++ meeting4/part1/makefile.cpp
@@ -1,1 +1,11 @@
+all:
+	g++ -o quadequationd quadequationd.cpp
 
+dump:
+	objdump -d quadequationd > quadequationd.asm
+
+clean:
+	rm -f quadequationd quadequationd.asm
+
+run: quadequationd
+	./quadequationd
