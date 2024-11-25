779d15ee6f09edd24ab112e300b20bd6f290668c
marvel568 <marvel.prabawa@gmail.com>
2024-11-25 16:22:31 +0700

    Update testfile.cpp

--- UAS/problem6/testfile.cpp
+++ UAS/problem6/testfile.cpp
@@ -1,33 +1,33 @@
 # Target to compile the object file
-finalexam_problem1: finalexam_problem1.o
-	g++ finalexam_problem1.o -o finalexam_problem1.exe
+UASno6: UASno6.o
+	g++ UASno61.o -o UASno6.exe
 
 # Target to compile the C++ program
-finalexam_problem1.o: finalexam_problem1.cpp
-	g++ -c finalexam_problem1.cpp -o finalexam_problem1.o
+UASno6.o: UASno6.cpp
+	g++ -c UASno6.cpp -o UASno6.o
 
 # Target to run the program (interactively)
-run: finalexam_problem1
-	./finalexam_problem1.exe
+run: UASno6
+	./UASno6.exe
 
 # Target to compile the object Test file
-finalexam_problem1_test: finalexam_problem1_test.o
-	g++ finalexam_problem1_test.o -o finalexam_problem1_test.exe
+UASno6: UASno6.o
+	g++ UASno6.o -o UASno6.exe
 
 # Target to compile the C++ Test program
-finalexam_problem1_test.o: finalexam_problem1_test.cpp
-	g++ -c finalexam_problem1_test.cpp -o finalexam_problem1_test.o
+UASno6.o: UASno6.cpp
+	g++ -c UASno6.cpp -o UASno6.o
 
 # Target to run the Test program
-test: finalexam_problem1_test
-	./finalexam_problem1_test.exe
+test: UASno6
+	./UASno6.exe
 
 # Target to clean up compiled files
 clean:
-	rm -f finalexam_problem1.o finalexam_problem1_test.o finalexam_problem1.exe finalexam_problem1_test.exe
+	rm -f UASno6.o UASno6.o UASno6.exe UASno6.exe
 
 all:
-	g++ -o finalexam_problem1 finalexam_problem1.cpp
+	g++ -o UASno6 UASno6.cpp
 
 dump:
-	objdump -d finalexam_problem1 > finalexam_problem1.asm
+	objdump -d UASno6 > UASno6.asm
